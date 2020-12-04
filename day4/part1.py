b05ba51de42d5fac3102cb5497317e83892a440b
Nicholas Chin <nic.c3.14@gmail.com>
2020-12-04 00:11:18 -0700

    Cleaned up day 4 solution
    Changed messy while loop with exceptions to use sys.stdin
    Replaced messy input parsing code with comprehensions

--- day4/part1.py
+++ day4/part1.py
@@ -1,28 +1,16 @@
-count = 0
-keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-while True:
-    line = input().split()
-    passport = dict()
-    try:
-        while len(line) != 0:
-            for field in line:
-                tmp = field.split(":")
-                passport[tmp[0]] = tmp[1]
-            line = input().split()
-        print(passport)
-        for key in keys:
-            if key not in passport.keys():
-                break
-        else:
-            count += 1
-        print(count)
-            
-    except EOFError:
-        for key in keys:
-            if key not in passport.keys():
-                break
-        else:
-            count += 1
-        break
+import sys
 
-print(count)
+
+def is_valid(passport):
+    keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    for key in keys:
+        if key not in passport.keys():
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    sections = [i.split() for i in "".join(list(sys.stdin)).split("\n\n")]
+    passports = [dict([i.split(":") for i in p]) for p in sections]
+    valid = [is_valid(p) for p in passports]
+    print(sum(valid))
--- day4/part2.py
+++ day4/part2.py
@@ -1,22 +1,22 @@
-count = 0
-keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+import sys
 
 
 def is_valid(passport):
+    keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
     for key in keys:
         if key not in passport.keys():
             return False
 
-        if key == "byr" and not (1920 <= int(passport[key]) <= 2002):
+        elif key == "byr" and not (1920 <= int(passport[key]) <= 2002):
             return False
 
-        if key == "iyr" and not (2010 <= int(passport[key]) <= 2020):
+        elif key == "iyr" and not (2010 <= int(passport[key]) <= 2020):
             return False
 
-        if key == "eyr" and not (2020 <= int(passport[key]) <= 2030):
+        elif key == "eyr" and not (2020 <= int(passport[key]) <= 2030):
             return False
 
-        if key == "hgt":
+        elif key == "hgt":
             if passport[key][-2:] == "in":
                 if not (59 <= int(passport[key][0:-2]) <= 76):
                     return False
@@ -26,7 +26,7 @@ def is_valid(passport):
             else:
                 return False
 
-        if key == "hcl":
+        elif key == "hcl":
             if len(passport[key]) == 7 and passport[key][0] == "#":
                 for c in passport[key][1:]:
                     if not ('a' <= c <= 'f') and not c.isdigit():
@@ -34,31 +34,18 @@ def is_valid(passport):
             else:
                 return False
 
-        if key == "ecl" and passport[key] not in ["amb", "blu", "brn", "gry", "grn",  "hzl", "oth"]:
+        elif key == "ecl" and passport[key] not in ["amb", "blu", "brn", "gry", "grn",  "hzl", "oth"]:
             return False
 
-        if key == "pid":
+        elif key == "pid":
             if len(passport[key]) != 9 or not passport[key].isdigit():
                 return False
 
     return True
 
 
-while True:
-    line = input().split()
-    passport = dict()
-    try:
-        while len(line) != 0:
-            for field in line:
-                tmp = field.split(":")
-                passport[tmp[0]] = tmp[1]
-            line = input().split()
-        print(passport)
-        count += 1 if is_valid(passport) else 0
-        print(count)
-            
-    except EOFError:
-        count += 1 if is_valid(passport) else 0
-        break
-
-print(count)
+if __name__ == "__main__":
+    sections = [i.split() for i in "".join(list(sys.stdin)).split("\n\n")]
+    passports = [dict([i.split(":") for i in p]) for p in sections]
+    valid = [is_valid(p) for p in passports]
+    print(sum(valid))
